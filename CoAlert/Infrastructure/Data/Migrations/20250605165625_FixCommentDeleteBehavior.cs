d9aa31ed8988b743f6de0d2c75a1da830af5d456
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2025-06-08 14:14:20 -0400

    Fix: Arrumando dockerfile

---
+++ CoAlert/Infrastructure/Data/Migrations/20250605165625_FixCommentDeleteBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoAlert.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCommentDeleteBehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CA_COMENTARIO_CA_COMENTARIO_ID_COMENTARIO_PARENTE",
+                table: "CA_COMENTARIO");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CA_COMENTARIO_CA_COMENTARIO_ID_COMENTARIO_PARENTE",
+                table: "CA_COMENTARIO",
+                column: "ID_COMENTARIO_PARENTE",
+                principalTable: "CA_COMENTARIO",
+                principalColumn: "ID_COMENTARIO",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CA_COMENTARIO_CA_COMENTARIO_ID_COMENTARIO_PARENTE",
+                table: "CA_COMENTARIO");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CA_COMENTARIO_CA_COMENTARIO_ID_COMENTARIO_PARENTE",
+                table: "CA_COMENTARIO",
+                column: "ID_COMENTARIO_PARENTE",
+                principalTable: "CA_COMENTARIO",
+                principalColumn: "ID_COMENTARIO",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
