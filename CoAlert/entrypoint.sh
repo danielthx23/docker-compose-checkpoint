c6534e1b00dd4d357fe34ee442532a44afea737e
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2025-08-29 14:08:30 -0400

    Feat: Adicionando Docker Compose

--- CoAlert/entrypoint.sh
+++ CoAlert/entrypoint.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -e
 
+host="$ORACLE_HOST"
+port="$ORACLE_PORT"
+
+echo "Esperando Oracle em $host:$port..."
+while ! (echo > /dev/tcp/$host/$port) >/dev/null 2>&1; do
+  sleep 5
+done
+
+echo "Oracle disponível, aguardando mais 10s..."
+sleep 10
+
+echo ">> Verificando dotnet..."
+which dotnet
+dotnet --version
+
+echo ">> Verificando dotnet-ef..."
+which dotnet-ef
+dotnet-ef --version
+
 if [ "$RUN_MIGRATIONS" = "true" ]; then
   echo ">> Rodando migrations..."
   dotnet ef database update --project ./src/CoAlert.csproj --verbose
